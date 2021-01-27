455a378c22bfa388d0985f378721d6dbfa11feb5
Eamonn O'Brien-Strain <eob@well.com>
2021-01-26 23:01:05 -0800

    Display build file

--- window.js
+++ window.js
@@ -1,8 +1,12 @@
 const { ipcRenderer } = require('electron')
 
-/* global fooElement */
+/* global Mustache, listElement, templateElement */
 
 // ipcRenderer.invoke('perform-action', ...args)
 ipcRenderer.on('buildfile', (event, buildfile) => {
-  fooElement.innerText = JSON.stringify(buildfile)
+  const template = templateElement.innerHTML
+  for (const target in buildfile) {
+    const html = Mustache.render(template, { target, ...buildfile[target] })
+    listElement.insertAdjacentHTML('beforeend', html)
+  }
 })
